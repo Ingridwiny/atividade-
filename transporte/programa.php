6e585679a52cde8b7a06ca9b89b7443182fe1182
Ingridwiny <ingrid_silva10@hotmail.com>
2013-12-18 16:19:04 -0200

    Create programa.php

---
+++ transporte/programa.php
@@ -0,0 +1,12 @@
+<?php
+$passagem = 0;
+$passagem = $_POST["passagem"];
+	if ($passagem <= 4){
+		echo "o valor que ira pagar " . number_format($passagem, 2, ",", ",");
+	 	echo $passagem * 1.40;
+}
+	
+	if ($passagem >= 5){
+		echo "o valor que ira pagar é R$ ";
+		echo ($passagem -4) * 2.80 + 5.60;
+}
