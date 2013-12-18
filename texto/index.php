335a9180c2c178a0542ebed5c84cc49ae8f9e484
Ingridwiny <ingrid_silva10@hotmail.com>
2013-12-18 16:22:40 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,17 @@
+<?php
+$x = 9;
+$y = 5;
+
+$dia = 11;
+$mes = 12;
+$ano = 2013;
+$texto = "a data de hoje é ";
+
+echo $texto . $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "a data de hoje é $dia/$mes/$ano";
+echo "<br/>";
+echo "$x + $y = " . ($x + $y);
+
+
+?>
